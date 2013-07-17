984876fa7f21d45c3e34b77ed59bf6ba256c8dc1
Jesse Baird <jebaird@gmail.com>
2013-07-17 09:06:28 -0500

    request animation frame for header graphic

--- _includes/js/scripts.js
+++ _includes/js/scripts.js
@@ -1,16 +1,20 @@
-/*
- * simple scripts
- */
 (function(){
-	
+window.requestAnimFrame = (function(){   
+	return window.requestAnimationFrame  ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame  ||
+		window.msRequestAnimationFrame  ||
+		function(/* function */ callback, /* DOMElement */ element){
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();  
 
-	 
- function drawHeader( options ){
+function drawHeader( options ){
 	 var defaults =  {
     	images: [ 
 			'/css/img/circle-1.png',
-			'/css/img/circle-1.png',
-			'/css/img/circle-1.png'
+			'/css/img/circle-2.png'
 		],
 		scale:{
 			min: 25,
@@ -22,7 +26,7 @@
 			'#585365',
 			'#CCCBD1'
 		],
-		numOfImg: 50,
+		numOfImg: 60,
 		imgPlacement: function(drawingContext, target, image){
 			var w = image.width * .50,
 				h = image.height * .50,
@@ -35,25 +39,21 @@
 				Y = this.rand( minY, maxY);
 				
 			drawingContext.drawImage(image, X, Y );
-		
 		}
 
 	};
-	
-	
 	var target = document.getElementById('header-graphic');
 	
 	target.width = document.body.clientWidth;
 	new jebaird.canvasImageRandomizer( target, defaults);
 }
+requestAnimFrame( drawHeader );
 
-
-drawHeader();
 //debounced
 var resizeTimer = null;
 window.onresize = function(){
 	clearTimeout( resizeTimer );
-	resizeTimer = setTimeout( drawHeader, 300 );
+	resizeTimer = setTimeout( requestAnimFrame( drawHeader ), 300 );
 }
 
 })();
