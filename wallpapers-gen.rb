f86937b96a63d3204ebd61ce541ef229c7d44d6c
Jesse Baird <jebaird@gmail.com>
2014-03-09 09:59:02 -0500

    added page for wallpappers

--- wallpapers-gen.rb
+++ wallpapers-gen.rb
@@ -23,3 +23,25 @@ images: [#{ imgs.join(',')}]
 ---\n
 "
     ) }
+
+ File.open("#{path}index.md", 'w') { |file| file.write(
+"---\n
+layout: photo-gallery
+title: Jesse Baird's curated collection of wallpapers
+images: [#{ imgs.join(',')}]
+use-echo: true
+---\n
+A set of hand picked wallpapers from various sources around the web. 
+
+Images prefixed with jebaird- are ones that Jesse Baird took.
+wallpaper- indicates that the image was downloaded from wallbase.cc
+All images are property of their owners.
+
+[view the rss theme](/wallpapers/index.xml)
+
+[get the windows 7 theme](/wallpapers/jebaird-collection.theme)
+
+
+"
+
+    ) }
